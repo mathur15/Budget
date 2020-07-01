fe425cac307c8cb95137b121260928689f91564e
mathur15 <som.mathur1997@gmail.com>
2020-07-01 19:01:35 -0400

    Set up event listener

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 var budgetController = (function(){
-          var x = 23
-          var add = function(a){
-          return x + a 
-          }
-          return{
-                //this is a way of making private functions
-                // and variables because of the closure created
-                //By doing this we provide a sort of public acces
-                //to stuff we want to give access to. 
-                publicTest:function(b){
-                    return (add(b))
-                }
-          }
+       
 })() 
+
+
 
 var UIController = (function(){
     //some code
 })()
+
 
 /*The reason the arguments are different from the original
 to prevent changing the name of the varibale in the function
 and only make those changes on the otuside.  */
 var appController = (function(budgerCtrl,UICtrl){
     //some code
-    var z = budgerCtrl.publicTest(5);
+    var ctrlAddItem =  function(){
 
-    return{
-        anotherPublicMethod: function(){
-            console.log(z)
-        }
     }
+    document.querySelector(".add_btn").addEventListener('click',function(){
+        ctrlAddItem()
+    })
+    //adding global eventlistener not for a specific item
+    document.addEventListener('keypress',function(event){
+        if(event.keyCode === 13){
+            ctrlAddItem()
+        }
+    })
+   
 })(budgetController,UIController);
