365fc72f36e2c5555ae87d19d34e81be2556bbe1
mathur15 <som.mathur1997@gmail.com>
2020-07-02 03:10:00 -0400

    Misspelt DOM tag for expenses

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var budgetController = (function(){
     var Expense = function(id,description,value){
         this.id = id;
@@ -61,7 +60,7 @@ var UIController = (function(){
         value: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer:'.income__list',
-        expenseContainer:'.expense__list'
+        expenseContainer:'.expenses__list'
     }
     return{
         getInputData:function(){
@@ -75,6 +74,8 @@ var UIController = (function(){
             return data
         },
         addListItem:function(obj,type){
+            //console.log(obj)
+            //console.log(type)
             var html,newHTML,elemet;
             // Create html string with placeholder text
             if(type === 'inc'){
@@ -83,19 +84,17 @@ var UIController = (function(){
             '<div class="right clearfix"><div class="item__value">+%num_value%</div><button class="item__delete--btn">'+
             '<i class="ion-ios-close-outline"></i></button></div></div></div>'
             }else if(type === 'exp'){
+                //set the container you want to be a part of - 'exp' or 'inc'
                 element = DOMstrings.expenseContainer
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div>'+
             '<div class="right clearfix"><div class="item__value">-%num_value%</div><div class="item__percentage">21%</div>'+
             '<div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>'+
             '</div></div></div>'
             }
-            console.log(element)
-            console.log(html)
             //replace the placeholder text with data
             newHTML = html.replace('%id%',obj.id)
             newHTML = newHTML.replace("%description%",obj.description)
             newHTML = newHTML.replace("%num_value%",obj.value)
-            console.log(newHTML)
             //Insert the html into the DOM
             //see documentation for insertAdjacentHTML
             document.querySelector(element).insertAdjacentHTML('beforeend',newHTML)
